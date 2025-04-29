ac5354753a0ce62c115d396730b3fee457d033f0
Husseinhajghazal <ist.husseinghajghazal@gmail.com>
2025-04-29 16:09:40 +0300

    a

--- backend/api/user/controller.js
+++ backend/api/user/controller.js
@@ -1,4 +1,5 @@
 const { createUser, deleteUser, getUsers, updateUser } = require("./service");
+
 const successResponse = require("../../utils/successRespnse");
 
 module.exports = {};
--- backend/api/user/service.js
+++ backend/api/user/service.js
@@ -1,59 +1,25 @@
-const ApiError = require("../../models/api-error");
 const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
 async function createUser(data) {
-  try {
-    return await prisma.user.create({ data });
-  } catch (e) {
-    console.log(e);
-    throw new ApiError("Error occured while creating user.", 400);
-  }
+  return await prisma.user.create({ data });
 }
 
 async function updateUser(id, data) {
-  try {
-    return await prisma.user.update({ data, where: { id } });
-  } catch (e) {
-    console.log(e);
-    throw new ApiError("Error occured while updating user.", 400);
-  }
+  return await prisma.user.update({ data, where: { id } });
 }
 
-async function getUser(id) {
-  let user;
-
-  try {
-    user = await prisma.user.findUnique({ where: { id } });
-  } catch (e) {
-    console.log(e);
-    throw new ApiError("Error occured while getting user.", 400);
-  }
-
-  if (user) {
-    return user;
+async function getUsers(id) {
+  if (id) {
+    return await prisma.user.findUnique({ where: { id } });
   } else {
-    throw new ApiError("User not found.", 404);
-  }
-}
-
-async function getUsers() {
-  try {
     return await prisma.user.findMany();
-  } catch (e) {
-    console.log(e);
-    throw new ApiError("Error occured while getting users.", 400);
   }
 }
 
 async function deleteUser(id) {
-  try {
-    return await prisma.user.delete({ where: { id } });
-  } catch (e) {
-    console.log(e);
-    throw new ApiError("Error occured while deleting user.", 400);
-  }
+  return await prisma.user.delete({ where: { id } });
 }
 
-module.exports = { createUser, updateUser, getUser, getUsers, deleteUser };
+module.exports = { createUser, updateUser, getUsers, deleteUser };
